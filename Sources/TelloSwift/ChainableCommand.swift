9870cca85522861e5ce8660177d21da423316bc3
Boris Gromov <0xff.root@gmail.com>
2020-06-30 18:14:45 +0200

    Fix: clean up command chain once commited, so it can't be reused

--- Sources/TelloSwift/ChainableCommand.swift
+++ Sources/TelloSwift/ChainableCommand.swift
@@ -31,18 +31,28 @@ public class ChainableCommand<Context, Output, Failure>: Publisher where Failure
     public let context: Context?
 
     private var trailing: AnyPublisher<Output, Failure>
+    private var committed: Bool
     private var subs: Set<AnyCancellable>
     private var semaphore: DispatchSemaphore = DispatchSemaphore(value: 1)
 
     internal func enqueue<Command>(command: Command) -> Self
         where Command: ExecutableCommand, Context == Command.Context, Output == Command.Output, Failure == Command.Failure
     {
-        trailing = trailing
-            .flatMap { _ in
-                CommandFuture { promise in
-                    command.execute(context: self.context, promise: promise)
-                }
-            }.eraseToAnyPublisher()
+        let future = CommandFuture { promise in
+            command.execute(context: self.context, promise: promise)
+        }
+
+        if committed {
+            // if committed, start the chain over
+            trailing = future.eraseToAnyPublisher()
+            committed = false
+        } else {
+            // otherwise, chain to the tail
+            trailing = trailing
+                .flatMap { _ in
+                    return future
+                }.eraseToAnyPublisher()
+        }
 
         return self
     }
@@ -68,6 +78,7 @@ public class ChainableCommand<Context, Output, Failure>: Publisher where Failure
     ///   - block: Closure that receives the `context`.
     ///
     public func async<S: Scheduler>(scheduler: S, block: @escaping (Context?) -> Void) -> Self {
+        // FIXME: Check if committed and ignore if so
         trailing = trailing
             .map { val in
                 scheduler.schedule {
@@ -102,6 +113,7 @@ public class ChainableCommand<Context, Output, Failure>: Publisher where Failure
     ///   - scheduler: Scheduler, e.g. `RunLoop` or `DispatchQueue`.
     ///   - block: Closure that receives the `context`.
     public func sync(block: @escaping (Context?) -> Void) -> Self {
+        // FIXME: Check if committed and ignore if so
         trailing = trailing
             .map { val in
                 block(self.context)
@@ -129,6 +141,7 @@ public class ChainableCommand<Context, Output, Failure>: Publisher where Failure
     /// stores the reference to a cancellable.
     public func commit() {
         trailing.sink(receiveCompletion: {_ in }, receiveValue: {_ in }).store(in: &subs)
+        committed = true
     }
 
     public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
@@ -142,6 +155,7 @@ public class ChainableCommand<Context, Output, Failure>: Publisher where Failure
     ///   - attemptToFulfill: Closure to execute.
     public init(context: Context? = nil, _ attemptToFulfill: @escaping (@escaping CommandPromise) -> Void) {
         self.context = context
+        committed = false
         trailing = CommandFuture(attemptToFulfill).eraseToAnyPublisher()
         subs = []
     }
